136c8df0cc1691ae556f9b3941d99c8ce78bb115
Mark_Zhang <zhangzn1990@hotmail.com>
2015-03-04 18:05:06 -0500

    small modification

---
+++ WePlan/WePlan/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  WePlan
+//
+//  Created by ZhangZhaonan on 15/3/4.
+//  Copyright (c) 2015年 WP Group. All rights reserved.
+//
+
+import Foundation
+
+class KanGou {
+    
+}
