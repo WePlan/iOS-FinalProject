e7139674f35ccac682c47e4154f0c4369b938537
chenkanck <chemkanck1990@gmail.com>
2015-03-04 18:02:24 -0500

    meanless 1

---
+++ WePlan/WePlan/GlobalColors.swift
@@ -0,0 +1,20 @@
+//
+//  GlobalColors.swift
+//  WePlan
+//
+//  Created by Kan Chen on 3/4/15.
+//  Copyright (c) 2015 WP Group. All rights reserved.
+//
+
+import Foundation
+
+// Class store all colors we will use
+class GlobalColors {
+    struct RGBA {
+        let name: String
+        let red: Float
+        let green: Float
+        let blue: Float
+        let alpha: Float
+    }
+}
