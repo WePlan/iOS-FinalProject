91964574237497bd7deade3caf8759fc82fd724e
Mark_Zhang <zhangzn1990@hotmail.com>
2015-04-09 16:43:58 -0400

    search

--- WePlan/WePlan/ParseFriend.swift
+++ WePlan/WePlan/ParseFriend.swift
@@ -7,8 +7,56 @@
 //
 
 import Foundation
+import Parse
 
 class ParseFriendAction : ParseFriend {
+    private struct FriendConstants{
+        static let friendClass = "Friends"
+        static let friendOne = "uid1"
+        static let friendTwo = "udi2"
+    }
+    
+    class func searchPeopleByNickname (nickname : String, complete : ([User]) -> Void) {
+        var resultList : [User] = []
+        var query = PFQuery(className: "_User")
+        query.whereKey("nickname", equalTo: nickname)
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                if let objects = objects as? [PFUser] {
+                    for object in objects {
+                        var item = User(name: object.objectForKey("nickname") as String, uemail: object.email)
+                        resultList.append(item)
+                    }
+                    complete(resultList)
+                }
+            }else {
+                println("Error \(error) \(error.userInfo)")
+            }
+        }
+    }
+    
+    class func searchPeopleByEmail (email : String, complete : ([User]) -> Void) {
+        var resultList : [User] = []
+        var query = PFQuery(className: "_User")
+        query.whereKey("email", equalTo: email)
+        query.findObjectsInBackgroundWithBlock {
+            (objects : [AnyObject]!, error : NSError!) -> Void in
+            if error == nil{
+                if let objects = objects as? [PFUser] {
+                    for object in objects {
+                        var item = User(name: object.objectForKey("nickname") as String, uemail: object.email)
+                        resultList.append(item)
+                    }
+                    complete(resultList)
+                }
+            }
+            else{
+                println("Error \(error) \(error.userInfo)")
+            }
+        }
+    }
+    
     
 }
 
