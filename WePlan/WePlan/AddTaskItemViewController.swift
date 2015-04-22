6885abe0cd3322b3962ef37a846c325c9853d0eb
chenkanck <chemkanck1990@gmail.com>
2015-04-22 16:49:50 -0400

    some try to animation, but not well

--- WePlan/WePlan/AddTaskItemViewController.swift
+++ WePlan/WePlan/AddTaskItemViewController.swift
@@ -15,10 +15,12 @@ class AddTaskItemViewController: UIViewController, UITextFieldDelegate {
     }
     @IBOutlet weak var taskTitleTextField: UITextField!
     
+    @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var dateLabel: UILabel!
     
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var pickerView: UIView!
     //Not implemented
     @IBOutlet weak var taskLocationTextField: UITextField!
     @IBOutlet weak var shortDescriptionTextField: UITextField!
@@ -52,11 +54,12 @@ class AddTaskItemViewController: UIViewController, UITextFieldDelegate {
     }
     //AbovePart Not Implementd
     
-    var format: NSDateFormatter = NSDateFormatter()
     
-    func datePickerDateChanged(datepicker: UIDatePicker) {
-        dateLabel.text = format.stringFromDate(datePicker.date)
-    }
+    //MARK: - DatePicker and its View
+    var format: NSDateFormatter = NSDateFormatter()
+    var pickerDisplayed: Bool = false
+    
+    
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -70,9 +73,52 @@ class AddTaskItemViewController: UIViewController, UITextFieldDelegate {
         let oneYearFromNow = now.dateByAddingTimeInterval(oneYearTime)
         datePicker.minimumDate = now
         datePicker.maximumDate = oneYearFromNow
-        
         datePicker.addTarget(self, action: "datePickerDateChanged:", forControlEvents: .ValueChanged)
     }
+    
+    func datePickerDateChanged(datepicker: UIDatePicker) {
+        dateLabel.text = format.stringFromDate(datePicker.date)
+    }
+    
+    func tapDateLabel() {
+        println("tapped")
+        if self.pickerDisplayed {
+            //hide View
+            hidePickerView()
+        }else{
+            //show View
+            showPickerView()
+        }
+    }
+    
+    func showPickerView(){
+        self.addButton.hidden = true
+        self.cancelButton.hidden = true
+        println("show")
+        let frame = self.view.frame
+        println(frame.height)
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.pickerView.frame = CGRectMake(0, frame.height, frame.width, 200)
+            }) { (finished:Bool) -> Void in
+            //
+            self.pickerDisplayed = true
+        }
+    }
+    
+    func hidePickerView(){
+        println("hide")
+        let frame = self.view.frame
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.pickerView.frame = CGRectMake(0, frame.height+200, frame.width, 0)
+            }) { (finished:Bool) -> Void in
+            //
+            self.pickerDisplayed = false
+            self.addButton.hidden = false
+            self.cancelButton.hidden = false
+        }
+        
+    }
+    //MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -86,19 +132,43 @@ class AddTaskItemViewController: UIViewController, UITextFieldDelegate {
     
         let current = datePicker.date
         dateLabel.text = format.stringFromDate(current)
+        dateLabel.userInteractionEnabled = true
+        let singleTap = UIGestureRecognizer(target: self, action: "tapDateLabel")
+        dateLabel.addGestureRecognizer(singleTap)
     }
     
     override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
         DefaultSetting.setNavigationBar(self.navigationController!)
+        
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        let size = self.view.frame.size
+        println(size)
+        println(pickerView.frame)
+        self.pickerView.frame = CGRectMake(0, size.height+200, 375, 0)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        taskTitleTextField.resignFirstResponder()
-    }
+//    
+//    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+//        let touch: UITouch = touches.first as! UITouch
+//        let touchPoint = touch.locationInView(self.view)
+//        let labelBounds = dateLabel.frame
+//        println(touchPoint)
+//        println(labelBounds)
+//        if labelBounds.contains(touchPoint){
+//            println("touched")
+//            tapDateLabel()
+//        }
+//        taskTitleTextField.resignFirstResponder()
+//    }
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
